5a2f2d26cbcf502f13a9f32b94d9a1fb20ee84e1
shaktimaan10 <43183000+shaktimaan10@users.noreply.github.com>
2019-12-07 03:58:39 -0500

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,32 @@
-(()=> {
+// planning for projects page
+
+//ai section
+// query selector for = hint event listener = click
+// on click = hint image shows
+//qury selector for = solution event listener = click
+// on click = movie name shows
+// add necessary display classes to main.css
+
+const hintImg = document.querySelectorAll(".hint-image"),
+      movieName = document.querySelectorAll(".movie-name"),
+      hint = document.querySelectorAll(".hint"),
+      forSolution = document.querySelectorAll(".solution");
+
+      hint.forEach((hintEach, index) => {
+          hintEach.addEventListener("click", function(){
+            hintImg[index].classList.add("hint-image-show"); // should show
+          })
+      })
+
+      forSolution.forEach((forSolutionEach, index) => {
+        forSolutionEach.addEventListener("click", function(){
+          movieName[index].classList.add("hint-image-show"); // should show
+        })
+    })
+//hint.addEventListener("click", function(){
     
-    const graphButton = document.querySelectorAll(".graph"),
-        popOver = document.querySelector(".popover");
-      
-    function buildPopover(information, el) {
-        popOver.querySelector(".topic").textContent = `${information.topic}`;
-        popOver.querySelector(".odds").textContent = `ODDS: ${information.odds}`;
-        popOver.querySelector(".description").textContent = `${information.description}`;
-        popOver.querySelector(".graph").src = `images/${information.graph}`;
+//});
 
-        //Image.src = 'images/cap-info.svg';
 
-        popOver.classList.toggle("show-popover");
-        if(popOver.classList.contains("show-popover")){
-            el.innerHTML = "";
-        } else {
-            el.innerHTML = "graph";
-        }
-        el.appendChild(popOver);
-    }
 
-    function fetchData() {
-        let targetEl = this,
-            url = `/svgdata/${this.dataset.target}`;
-
-            fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                console.log(targetEl);
-
-                //populate the popover
-                buildPopover(data, targetEl);
-            })
-            .catch((err) => console.log(err));
-      }
-
-    graphButton.forEach(button => button.addEventListener("click", fetchData));
-})();
+// planning for contact page
